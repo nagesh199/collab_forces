5ce424bcf50c0a0abe4d76238791b41767ccfaf9
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-28 21:49:12 +0530

    backend

--- stock/src/App.js
+++ stock/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
-//import io from "socket.io-client"
+import io from "socket.io-client"
 import './App.css';
-//import {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  // const socket = io.connect("http://localhost:8080");
-  // const [stockData,setStockData] = useState([]);
+  const socket = io.connect("http://localhost:8080");
+  const [stockData,setStockData] = useState([]);
   
-  // useEffect(()=>{
-  //   getdata()
-  // },[])
-  // const getdata = ()=>{
-  //   socket.on("stock",(payload)=>{
-  //     setStockData(payload.indexdata)
-  //   });
+  useEffect(()=>{
+    getdata()
+  },[])
+  const getdata = ()=>{
+    socket.on("stock",(payload)=>{
+      setStockData(payload.indexdata)
+    });
     
-  // }
+  }
   // console.log(stockData)
   return (
     <div className="App">
-       
+       {stockData.map((el)=>{
+         return <h1>{(el.current)+(Math.random()*10)}</h1>
+       })}
     </div>
   );
 }
