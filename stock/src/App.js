25fe62c2bbea89e843d33e4c16efb6db0211576a
spaswan141 <paswanshubham0309@outlook.com>
2022-07-28 21:41:21 +0530

    commit

--- stock/src/App.js
+++ stock/src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-import Footer from './components/Footer';
-import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
-    <Footer/>
+  
     </div>
   );
 }
