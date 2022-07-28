63ba70db1ed1461be6539c6f349e450af25706c4
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-28 20:19:46 +0530

    add backend

--- stock/src/App.js
+++ stock/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
+//import io from "socket.io-client"
 import './App.css';
+//import {useEffect, useState} from "react";
 
 function App() {
+  // const socket = io.connect("http://localhost:8080");
+  // const [stockData,setStockData] = useState([]);
+  
+  // useEffect(()=>{
+  //   getdata()
+  // },[])
+  // const getdata = ()=>{
+  //   socket.on("stock",(payload)=>{
+  //     setStockData(payload.indexdata)
+  //   });
+    
+  // }
+  // console.log(stockData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
     </div>
   );
 }
