c48948950f9b98f4b0581fcc74c562cb67eaef05
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-29 15:49:10 +0530

    frontend

--- stock/src/App.js
+++ stock/src/App.js
@@ -1,15 +1,11 @@
 
 import './App.css';
-import Footer from './components/Footer';
-import Home from './pages/Home';
 
 function App() {
   
   return (
     <div className="App">
-      <Home/>
-      <Footer/>
-         
+     
     </div>
   );
 }
--- stock/src/components/chart.jsx
+++ stock/src/components/chart.jsx
@@ -1,11 +1,25 @@
 
-// import React from "react";
-// import io from "socket.io-client"
-// import 
-// export const Chart = ()=>{
-//     const socket = io.connect("http://localhost:8080");
+import React from "react";
+import io from "socket.io-client"
+import {Line,CartesianGrid,LineChart,XAxis,YAxis,Tooltip} from "recharts"
+import { useEffect,useState } from "react";
 
-//     return (
-        
-//     )
-// }
+export const Chart = ()=>{
+    const socket = io.connect("http://localhost:8080");
+   const [chartData,setChartData] = useState([])
+    useEffect(()=>{
+        socket.on("cpu",(cpuPercent)=>{
+            setChartData((currentData)=>[...currentData,cpuPercent])
+        })
+    },[])
+    return (
+        <div>
+               <LineChart width={500} height={300} data={chartData}>
+                    <XAxis dataKey="name"/>
+                    <YAxis/>
+                    <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+                    <Line dataKey="value" />
+              </LineChart>
+        </div>
+    )
+}
